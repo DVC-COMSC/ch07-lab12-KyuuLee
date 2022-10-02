6313c5402d345bb3a9a792e69c09642f036687e5
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-10-02 15:05:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+
+from functools import reduce
+
+
 numbers = [     [10, 11, 12, 13, 14],
                [20, 21, 22, 23, 24],
                [30, 31, 32, 33, 34]]
 
-# numbers =[] 
-# rnum = int(input('Enter the number of rows'))
-# for i in range(rnum):
-# 	row = list(map(int, input('Enter 5 values for a row: ').split()))
-# 	numbers.append(row)
 
-rnum = len(numbers)
-cnum = len(numbers[0])
+for i in range(len(numbers[0])):
+       csum = 0
+       for j in range(len(numbers)):
+               csum += numbers[j][i]
+       print ("The summation of column {0}: {1}".format(i, csum))
 
-# ******************************
-# Make your Code
-# ******************************
+
+# sum(list(zip(numbers[0], numbers[1], numbers[2]))[0])
+# sum(list(zip(numbers[0], numbers[1], numbers[2]))[1])
+# sum(list(zip(numbers[0], numbers[1], numbers[2]))[2])
+# sum(list(zip(numbers[0], numbers[1], numbers[2]))[3])
+# sum(list(zip(numbers[0], numbers[1], numbers[2]))[4])
